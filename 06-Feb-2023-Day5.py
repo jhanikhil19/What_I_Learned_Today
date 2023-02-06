e85545184034c5718258cd95d2490d6f6f30ae26
Nikhil Jha <jhanikhil19@gmail.com>
2023-02-06 11:57:48 +0530

    Create 06-Feb-2023-Day5.py

---
+++ 06-Feb-2023-Day5.py
@@ -0,0 +1,14 @@
+dd="060223"
+print("Learning for 6-Feb-2023 Day 5")
+print(" We need to plan and organise our life at our own. No one else cares about you.")
+print("Being selfish for own growth is not bad, but I don't mean to push or throw others under the bus. Its just focus on self first. ")
+print("Helping is a great thing. But, in this selfish world - don't give your all to someone else.")
+print("The time you are thinking is coming for free, is actually not coming for free.")
+print("So, think twice before doing that. ")
+
+
+print("Best Regards,")
+print("Nikhil Jha")
+print("Paderborn")
+print(dd[0,2],dd[2,4],dd[4,6])
+
