b2fc31dd0799e4d55331952c92c18e328a53de8a
Nikhil Jha <jhanikhil19@gmail.com>
2023-02-08 15:18:07 +0530

    Create 08-Feb-2023-Day7.py

---
+++ 08-Feb-2023-Day7.py
@@ -0,0 +1,11 @@
+dd="080223"
+print("Learning for 8-Feb-2023 Day 7")
+print(" More than 10K people lost their life in Turkey and Syria")
+print("Need to take control of my head. Its just wobling around too much. ")
+print("Work on the things which are importanat, even if you are lazy. ")
+
+
+print("Best Regards,")
+print("Nikhil Jha")
+print("Paderborn")
+print(dd[0,2],dd[2,4],dd[4,6])
