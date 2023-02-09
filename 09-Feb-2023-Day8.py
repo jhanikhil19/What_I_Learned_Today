b56b2d85c78858f507804f4d9e490e81f165a513
Nikhil Jha <jhanikhil19@gmail.com>
2023-02-09 16:07:32 +0530

    Create 09-Feb-2023-Day8.py

---
+++ 09-Feb-2023-Day8.py
@@ -0,0 +1,12 @@
+dd="090223"
+print("Learning for 9-Feb-2023 Day 8")
+print("Don't try to change anyone. ")
+print("Focus on your work.")
+print("Live life like an example.")
+
+
+print("Best Regards,")
+print("Nikhil Jha")
+print("Paderborn")
+print(dd[0,2],dd[2,4],dd[4,6])
+
