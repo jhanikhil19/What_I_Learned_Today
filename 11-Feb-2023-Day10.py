43e9babae3988ae9ff58d2e769f59faa87805ec3
Nikhil Jha <jhanikhil19@gmail.com>
2023-02-11 13:20:27 +0530

    Create 11-Feb-2023-Day10.py

---
+++ 11-Feb-2023-Day10.py
@@ -0,0 +1,11 @@
+dd="110223"
+print("Learning for 11-Feb-2023 Day 10")
+print("We did not stopped playing because we grow old. But we grow old because we stopped playing")
+print("Exams will come and go. Read and study for understanding, not only for the marks and rank.")
+print("SLEEP + MOVE + SUNLIGHT + NUTRITION + SOCIAL CONNECTION = LONGER HEALTHIER LIFE ")
+
+
+print("Best Regards,")
+print("Nikhil Jha")
+print("Paderborn")
+print(dd[0,2],dd[2,4],dd[4,6])
