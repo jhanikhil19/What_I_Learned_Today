6c17fa526cc21bd4dae878b957dd0037022db3a2
Nikhil Jha <jhanikhil19@gmail.com>
2023-02-12 12:32:25 +0530

    Create 12-Feb-2023-Day11.py

---
+++ 12-Feb-2023-Day11.py
@@ -0,0 +1,11 @@
+dd="120223"
+print("Learning for 12-Feb-2023 Day 11")
+print("Stop thinking about the things not under your control, else you will start loosing the control over things you can control.")
+print("Always give yourself a high five in the mirror when you go to washroom")
+print("Stay with positive people.")
+
+
+print("Best Regards,")
+print("Nikhil Jha")
+print("Paderborn")
+print(dd[0,2],dd[2,4],dd[4,6])
