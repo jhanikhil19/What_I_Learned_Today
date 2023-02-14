575a0534ba3aa6025ab2339fabe69ddf074222b7
Nikhil Jha <jhanikhil19@gmail.com>
2023-02-14 12:34:35 +0530

    Create 13-Feb-2023-Day12.py

---
+++ 13-Feb-2023-Day12.py
@@ -0,0 +1,11 @@
+dd="130223"
+print("Learning for 13-Feb-2023 Day 12")
+print("Work daily and then only wait for the success or the result.")
+print("Never expect something big or good to happen only after working for a day on it. Or the last moment.")
+print("Make discipline your habit ")
+
+
+print("Best Regards,")
+print("Nikhil Jha")
+print("Paderborn")
+print(dd[0,2],dd[2,4],dd[4,6])
