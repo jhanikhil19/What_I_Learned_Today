6eac1ef1d0f9f225fd835cd581d9a70499a4c4bb
Nikhil Jha <jhanikhil19@gmail.com>
2023-02-15 20:47:48 +0530

    Create 14-Feb-2023-Day13.py

---
+++ 14-Feb-2023-Day13.py
@@ -0,0 +1,11 @@
+dd="140223"
+print("Learning for 14-Feb-2023 Day 13")
+print("Good things lie on the other side of fear")
+print("DDIE was hard but now it's going good.")
+print("Take rest for a day and start working from next day. ")
+
+
+print("Best Regards,")
+print("Nikhil Jha")
+print("Paderborn")
+print(dd[0,2],dd[2,4],dd[4,6])
