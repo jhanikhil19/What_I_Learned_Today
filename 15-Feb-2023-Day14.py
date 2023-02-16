de3fbf74ad06c4d7ba935e8328fbe271c3cdf543
Nikhil Jha <jhanikhil19@gmail.com>
2023-02-17 01:53:08 +0530

    Create 15-Feb-2023-Day14.py

---
+++ 15-Feb-2023-Day14.py
@@ -0,0 +1,10 @@
+dd="150223"
+print("Learning for 15-Feb-2023 Day 14")
+print("Work on your self.")
+print("Learn from others.")
+print("Rest in the end. Not in the middle.")
+
+print("Best Regards,")
+print("Nikhil Jha")
+print("Paderborn")
+print(dd[0,2],dd[2,4],dd[4,6])
